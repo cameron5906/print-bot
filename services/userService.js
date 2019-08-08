8c3fc95f6025136c3f8de1c3c48418668e7c7140
ccooper <ccooper@cars.com>
2019-08-08 14:16:25 -0400

    File processing stuff

---
+++ services/userService.js
@@ -0,0 +1,32 @@
+const SlackWeb = require('../wrappers/slack');
+const users = require('../users.json');
+
+class UserService {
+    constructor() {
+        this.userIdCache = {};
+    }
+
+    async getRole(userId) {
+        const username = await this._getUsernameFromId(userId);
+        const userInfo = users.find(u => u.username === username);
+
+        if(!userInfo) return false;
+        return userInfo.role;
+    }
+
+    async isAllowed(userId) {
+        const username = await this._getUsernameFromId(userId);
+
+        return typeof(users.find(u => u.username === username)) !== 'undefined';
+    }
+
+    async _getUsernameFromId(userId) {
+        if(this.userIdCache[userId]) return this.userIdCache[userId];
+        const {username} = await SlackWeb.getUserInfo(userId);
+
+        this.userIdCache[userId] = username;
+        return username;
+    }
+}
+
+module.exports = new UserService;
